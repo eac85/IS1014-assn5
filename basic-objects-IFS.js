1630c27f7e8ff7e3bab14f0026cb1aa14282e520
Emery <eac85@pitt.edu>
2018-03-20 19:54:40 -0400

    no texture

--- basic-objects-IFS.js
+++ basic-objects-IFS.js
@@ -70,6 +70,7 @@ function cube(side) {
  * @slices the number of radial subdivisions in the circular approximation
  *    of an annulus.  If not provided, the value will be 32.
  */
+ //ring function to make figure-8 out of 
 function ring(innerRadius, outerRadius, slices) {
    if (arguments.length == 0)
       innerRadius = 0.25;
@@ -143,6 +144,8 @@ function ring(innerRadius, outerRadius, slices) {
        indices: indices
    };
 }
+
+
 
 /**
  * Create a model of a sphere.  The z-axis is the axis of the sphere,
@@ -223,6 +226,66 @@ function uvTorus(outerRadius, innerRadius, slices, stacks) {
    slices = slices || 32;
    stacks = stacks || 16;
    var vertexCount = (slices+1)*(stacks+1);
+   var vertices = new Float32Array( 3*vertexCount );
+   var normals = new Float32Array( 3* vertexCount );
+   var texCoords = new Float32Array( 2*vertexCount );
+   var indices = new Uint16Array( 2*slices*stacks*3 );
+   var du = 2*Math.PI/slices;
+   var dv = 2*Math.PI/stacks;
+   var centerRadius = (innerRadius+outerRadius)/2;
+   var tubeRadius = outerRadius - centerRadius;
+   var i,j,u,v,cx,cy,sin,cos,x,y,z;
+   var indexV = 0;
+   var indexT = 0;
+   for (j = 0; j <= stacks; j++) {
+      v = -Math.PI + j*dv;
+      cos = Math.cos(v);
+      sin = Math.sin(v);
+      for (i = 0; i <= slices; i++) {
+         u = i*du;
+         cx = Math.cos(u);
+         cy = Math.sin(u);
+         x = cx*(centerRadius + tubeRadius*cos);
+         y = cy*(centerRadius + tubeRadius*cos);
+         z = sin*tubeRadius;
+         vertices[indexV] = x;
+         normals[indexV++] = cx*cos;
+         vertices[indexV] = y
+         normals[indexV++] = cy*cos;
+         vertices[indexV] = z
+         normals[indexV++] = sin;
+         texCoords[indexT++] = i/slices;
+         texCoords[indexT++] = j/stacks;
+      } 
+   }
+   var k = 0;
+   for (j = 0; j < stacks; j++) {
+      var row1 = j*(slices+1);
+      var row2 = (j+1)*(slices+1);
+      for (i = 0; i < slices; i++) {
+          indices[k++] = row1 + i;
+          indices[k++] = row2 + i + 1;
+          indices[k++] = row2 + i;
+          indices[k++] = row1 + i;
+          indices[k++] = row1 + i + 1;
+          indices[k++] = row2 + i + 1;
+      }
+   }
+   return {
+       vertexPositions: vertices,
+       vertexNormals: normals,
+       vertexTextureCoords: texCoords,
+       indices: indices
+   };
+}
+
+
+function other(outerRadius, innerRadius, slices, stacks) {
+   outerRadius = outerRadius || 0.5;
+   innerRadius = innerRadius || outerRadius/3;
+   slices = slices || 32;
+   stacks = stacks || 16;
+   var vertexCount = (slices+4)*(stacks+4);
    var vertices = new Float32Array( 3*vertexCount );
    var normals = new Float32Array( 3* vertexCount );
    var texCoords = new Float32Array( 2*vertexCount );
