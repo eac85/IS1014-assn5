f0d5b55e03773ca240970d0c6f31bdfb80f12611
Emery <eac85@pitt.edu>
2018-03-25 15:12:05 -0400

    fogure8

--- basic-objects-IFS.js
+++ basic-objects-IFS.js
@@ -298,11 +298,12 @@ function other(outerRadius, innerRadius, slices, stacks) {
    var indexV = 0;
    var indexT = 0;
    for (j = 0; j <= stacks; j++) {
+      var uoffset = (j % 2 == 0? 0 : 0.5);
       v = -Math.PI + j*dv;
       cos = Math.cos(v);
       sin = Math.sin(v);
       for (i = 0; i <= slices; i++) {
-         u = i*du;
+         u = (i+uoffset)*du;
          cx = Math.cos(u);
          cy = Math.sin(u);
          x = cx*(centerRadius + tubeRadius*cos);
@@ -314,7 +315,7 @@ function other(outerRadius, innerRadius, slices, stacks) {
          normals[indexV++] = cy*cos;
          vertices[indexV] = z
          normals[indexV++] = sin;
-         texCoords[indexT++] = i/slices;
+         texCoords[indexT++] = (i+0.5)/slices;
          texCoords[indexT++] = j/stacks;
       } 
    }
@@ -388,71 +389,56 @@ function other(outerRadius, innerRadius, slices, stacks) {
 
 
 
-function newring(innerRadius, outerRadius, slices) {
-   if (arguments.length == 0)
-      innerRadius = 0.25;
-   outerRadius = outerRadius || innerRadius * 2 || 0.5;
+function newring(outerRadius, innerRadius, slices, stacks) {
+  outerRadius = outerRadius || 0.5;
+   innerRadius = innerRadius || outerRadius/3;
    slices = slices || 32;
-   var vertexCount, vertices, normals, texCoords, indices, i;
-   vertexCount = (innerRadius == 0)? slices + 1 : slices * 2;
-   vertices = new Float32Array( 3*vertexCount );
-   normals = new Float32Array( 3* vertexCount );
-   texCoords = new Float32Array( 2*vertexCount );
-   indices = new Uint16Array( innerRadius == 0 ?  3*slices : 3*2*slices );
-   var d = 2*Math.PI/slices;
+   stacks = stacks || 16;
+   var vertexCount = (slices+1)*(stacks+1);
+   var vertices = new Float32Array( 3*vertexCount );
+   var normals = new Float32Array( 3* vertexCount );
+   var texCoords = new Float32Array( 2*vertexCount );
+   var indices = new Uint16Array( 2*slices*stacks*3 );
+   var du = 2*Math.PI/slices;
+   var dv = 2*Math.PI/stacks;
+   var centerRadius = (innerRadius+outerRadius)/2;
+   var tubeRadius = outerRadius - centerRadius;
+   var i,j,u,v,cx,cy,sin,cos,x,y,z;
+   var indexV = 0;
+   var indexT = 0;
+   for (j = 0; j <= stacks; j++) {
+      v = -Math.PI + j*dv;
+      cos = Math.cos(v);
+      sin = Math.sin(v);
+      for (i = 0; i <= slices; i++) {
+         u = i*du;
+         cx = Math.cos(u);
+         cy = Math.sin(2*u);
+         x = cx*(centerRadius + tubeRadius*cos);
+         y = cy*(centerRadius + tubeRadius*cos);
+         z = sin*tubeRadius;
+         vertices[indexV] = x;
+         normals[indexV++] = cx*cos;
+         vertices[indexV] = y
+         normals[indexV++] = cy*cos;
+         vertices[indexV] = z
+         normals[indexV++] = sin;
+         texCoords[indexT++] = i/slices;
+         texCoords[indexT++] = j/stacks;
+      } 
+   }
    var k = 0;
-   var t = 0;
-   var n = 0;
-   if (innerRadius == 0) {
+   for (j = 0; j < stacks; j++) {
+      var row1 = j*(slices+1);
+      var row2 = (j+1)*(slices+1);
       for (i = 0; i < slices; i++) {
-         c = Math.cos(d*i);
-         s = Math.sin(d*i);
-         vertices[k++] = c*outerRadius;
-         vertices[k++] = s*outerRadius;
-         vertices[k++] = 0;
-         texCoords[t++] = 0.5 + 0.5*c;
-         texCoords[t++] = 0.5 + 0.5*s;
-         indices[n++] = slices;
-         indices[n++] = i;
-         indices[n++] = i == slices-1 ? 0 : i + 1;
+          indices[k++] = row1 + i;
+          indices[k++] = row2 + i + 1;
+          indices[k++] = row2 + i;
+          indices[k++] = row1 + i;
+          indices[k++] = row1 + i + 1;
+          indices[k++] = row2 + i + 1;
       }
-      vertices[k++] = vertices[k++] = vertices[k++] = 0;
-      texCoords[t++] = texCoords[t++] = 0;
-   }
-   else {
-      var r = innerRadius / outerRadius;
-      for (i = 0; i < slices; i++) {
-         c = Math.cos(d*i);
-         s = Math.sin(2*d*i);
-         vertices[k++] = c*innerRadius;
-         vertices[k++] = s*innerRadius;
-         vertices[k++] = 0;
-         texCoords[t++] = 0.5 + 0.5*c*r;
-         texCoords[t++] = 0.5 + 0.5*s*r;
-         vertices[k++] = c*outerRadius;
-         vertices[k++] = s*outerRadius;
-         vertices[k++] = 0;
-         texCoords[t++] = 0.5 + 0.5*c;
-         texCoords[t++] = 0.5 + 0.5*s;
-      }
-      for (i = 0; i < slices - 1; i++) {
-         indices[n++] = 2*i;
-         indices[n++] = 2*i+1;
-         indices[n++] = 2*i+3;
-         indices[n++] = 2*i;
-         indices[n++] = 2*i+3;
-         indices[n++] = 2*i+2;
-      }
-      indices[n++] = 2*i;
-      indices[n++] = 2*i+1;
-      indices[n++] = 1;
-      indices[n++] = 2*i;
-      indices[n++] = 1;
-      indices[n++] = 0;
-   }
-   for (i = 0; i < vertexCount; i++) {
-      normals[3*i] = normals[3*i+1] = 0;
-      normals[3*i+2] = 1;
    }
    return {
        vertexPositions: vertices,
